23acd2c0123caa480e4ec8305a5c75c70bac211c
Dannie55 <ezirimdaniel080@gmail.com>
2023-03-27 04:03:44 -0700

    exception

---
+++ 0x05-python-exceptions/101-safe_function.py
@@ -0,0 +1,9 @@
+#!/bin/usr/python3
+def safe_function(fct, *args):
+    try:
+        r = fct(*args)
+        return r
+    except Exception as error:
+        import sys
+        print("Exception: {}".format(error), file=sys.stderr)
+        return None
