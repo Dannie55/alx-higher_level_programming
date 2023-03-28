c348df5c9a22a06430115c1033c9c2961d038ef3
Dannie55 <ezirimdaniel080@gmail.com>
2023-03-28 04:37:53 -0700

    classes

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -1,20 +1,21 @@
 #!/usr/bin/python3
+
 """defines a class Square"""
 
 
 class square:
-	"""Represent a square
-	Attributes:
-	    __size (int): size of side
-	"""
-	def __int__(self, size=0):
-	    """intiialization of sqaure
+    """Represent a square
+        Attributes:
+    __size (int): size of side
+    """
+    def __int__(self, size=0):
+        """intiialization of sqaure
             Args:
-	        size (int): size of sides
-            Returns:
+        size (int): size of sides
+        Returns:
                 None
-           """
-	   if type(size) is not int:
+        """
+        if type(size) is not int:
             raise TypeError("size must be an integer")
         else:
             if size < 0:
