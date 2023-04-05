b92d9d1b17533a6a6c4a973fab7fd9b518e5be3e
Dannie55 <ezirimdaniel080@gmail.com>
2023-04-05 00:12:18 -0700

    fixed bugs

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
 def add_integer(a, b=98):
-    """Return the sum of two numbers"""
+    """Function that sums up 2 numbers"""
 
     if not a or (type(a) != int and type(a) != float):
         raise TypeError("a must be an integer")
-    if not b (type(b) != int and type(b) != float:
+    if type(b) != int and type(b) != float:
         raise TypeError("b must be an integer")
-    
+
     return(int(a) + int(b))
