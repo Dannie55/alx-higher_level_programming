70c29cfdc5c88b9e07170ab4460aedc571848434
Dannie55 <ezirimdaniel080@gmail.com>
2023-04-11 05:47:54 -0700

    subclass

---
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""Defines a class inherited directly of indirectly from a specified class"""
+
+
+def inherits_from(obj, a_class):
+    """Returns true if inherited directly or indirectly from a specified class
+    Args:
+        obj(any): The object of a class.
+        a_class(type): matched to the object
+    Returns:
+        if the obj is  inherited directly or indirectly - True
+        Otherwise return- False.
+    """
+    if issubclass(obj, a_class):
+        return True
+    return False
