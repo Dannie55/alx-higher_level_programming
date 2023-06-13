c501a42d95478366d8720e96b265c48407e9f265
Dannie55 <ezirimdaniel080@gmail.com>
2023-06-13 11:56:33 +0000

    javascript warm up files

--- 0x12-javascript-warm_up/12-object.js
+++ 0x12-javascript-warm_up/12-object.js
@@ -1,12 +1,8 @@
 #!/usr/bin/node
-const args = process.argv.slice(2).map(Number);
-
-if (args.length === 0 || args.length === 1) {
-  console.log(0);
-} else {
-  const updatedArgs = args.map(num => (num === 12 ? 89 : num));
-  const max = Math.max(...updatedArgs);
-  const secondMax = Math.max(...updatedArgs.filter(num => num < max));
-  console.log(secondMax);
-}
-
+const myObject = {
+  type: 'object',
+  value: 12
+};
+console.log(myObject);
+myObject.value = 89;
+console.log(myObject);
