614fc3479dccf984a5a1fa9a2a4b5f02c75bab50
Dannie55 <ezirimdaniel080@gmail.com>
2023-06-13 11:49:39 +0000

    javascript warm up files

--- 0x12-javascript-warm_up/10-factorial.js
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -11,8 +11,7 @@ function factorial(n) {
   return n * factorial(n - 1);
 }
 
-const num = parseInt(process.argv[2]);
+const num = Number(process.argv[2]);
 const result = factorial(num);
 
 console.log(result);
-
