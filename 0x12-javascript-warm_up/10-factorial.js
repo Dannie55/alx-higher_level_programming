ca745b5ca446cec2e8a21f2caf5d2bf002eeb206
Dannie55 <ezirimdaniel080@gmail.com>
2023-06-13 11:52:25 +0000

    javascript warm up files

--- 0x12-javascript-warm_up/10-factorial.js
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -1,15 +1,6 @@
 #!/usr/bin/node
 function factorial(n) {
-  if (isNaN(n)) {
-    return 1;
-  }
-  
-  if (n === 0 || n === 1) {
-    return 1;
-  }
-  
-  return n * factorial(n - 1);
-}
+  return n === 0 || isNaN(n) ? : n * factorial(n - 1);
 
 const num = Number(process.argv[2]);
 const result = factorial(num);
