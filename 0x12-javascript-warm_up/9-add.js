dd2b397dc8810363275a7489763b12aa6b32cdd2
Dannie55 <ezirimdaniel080@gmail.com>
2023-06-13 11:45:21 +0000

    javascript warm up files

--- 0x12-javascript-warm_up/9-add.js
+++ 0x12-javascript-warm_up/9-add.js
@@ -2,14 +2,4 @@
 function add(a, b) {
   return a + b;
 }
-
-const arg1 = parseInt(process.argv[2]);
-const arg2 = parseInt(process.argv[3]);
-
-if (!isNaN(arg1) && !isNaN(arg2)) {
-  const result = add(arg1, arg2);
-  console.log(result);
-} else {
-  console.log("Invalid arguments");
-}
-
+console.log(add(Number(process.argv[2]), Number(process.argv[3])));
