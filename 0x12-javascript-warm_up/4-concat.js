da1eb0cc83c5c82eed5d8e0c3af1a6cd58074170
Dannie55 <ezirimdaniel080@gmail.com>
2023-06-13 11:27:19 +0000

    javascript warm up files

--- 0x12-javascript-warm_up/4-concat.js
+++ 0x12-javascript-warm_up/4-concat.js
@@ -1,7 +1,2 @@
 #!/usr/bin/node
-if (process.argv[2] === undefined || process.argv[3] === undefined) {
-  console.log("Invalid arguments");
-} else {
-  console.log(`${process.argv[2]} is ${process.argv[3]}`);
-}
-
+console.log(`${process.argv[2]} is ${process.argv[3]}`);
