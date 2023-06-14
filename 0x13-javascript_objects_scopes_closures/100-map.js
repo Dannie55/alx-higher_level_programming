57842c01120be5e07d85f230cf412183b8d8130c
Dannie55 <ezirimdaniel080@gmail.com>
2023-06-14 17:31:08 +0000

    javascript objevt

--- 0x13-javascript_objects_scopes_closures/100-map.js
+++ 0x13-javascript_objects_scopes_closures/100-map.js
@@ -1,6 +1,4 @@
-const { list } = require('./100-data');
-
-const newList = list.map((value, index) => value * index);
-
-console.log("Initial List:", list);
-console.log("New List:", newList);
+#!/usr/bin/node
+const list = require('./100-data.js').list;
+console.log(list);
+console.log(list.map((item, index) => item * index));
