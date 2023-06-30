64ace9fb7595c0ae2ada6436b641dbff7517af47
Dannie55 <daniel080@gmail.com>
2023-06-30 19:11:17 +0100

    network

--- 0x10-python-network_0/5-post_params.sh
+++ 0x10-python-network_0/5-post_params.sh
@@ -1,11 +1,4 @@
 #!/bin/bash
 
-URL="$1"
-EMAIL="test@gmail.com"
-SUBJECT="I will always be here for PLD"
-
 # Send POST request and store response
-RESPONSE=$(curl -s -X POST -d "email=$EMAIL&subject=$SUBJECT" "$URL")
-
-# Display response body
-echo "$RESPONSE"
+curl -s -X POST -d "email=test@gmail.com&subject=I will always be here for PLD" "$1"
