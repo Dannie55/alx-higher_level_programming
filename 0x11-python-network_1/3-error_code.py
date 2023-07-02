242b40984e5077c7d36bca3f2383c7224425e5c1
Dannie55 <ezirimdaniel080@gmail.com>
2023-07-01 17:20:14 -0700

    network

--- 0x11-python-network_1/3-error_code.py
+++ 0x11-python-network_1/3-error_code.py
@@ -3,6 +3,9 @@
 Python script that takes in a URL, sends a request to the URL and displays
 the body of the response (decoded in utf-8).
 """
+
+
+
 import urllib.request
 import urllib.error import HTTPError
 import sys
@@ -10,7 +13,6 @@ import sys
 
 if __name__ == "__main__":
     url = sys.argv[1]
-
     try:
         with urllib.request.urlopen(url) as response:
             body = response.read().decode('utf-8')
