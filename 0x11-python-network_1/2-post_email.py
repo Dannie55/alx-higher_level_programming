a1cc523b6f5e1a6e89960769cc5c63f153d09463
Dannie55 <ezirimdaniel080@gmail.com>
2023-07-01 17:05:35 -0700

    network

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 """
-Python script that takes in a URL and an email, sends a POST request to the 
-and displays the body of the response (decoded in utf-8)
+Python script that takes in a URL and an email displays the body of the
+response (decoded in utf-8)
 """
 import urllib.request
 import urllib.parse
